2000273116cf5ce2c62457250257d3c62eb1bba1
Guy Ponimansky <ponimansky.guy@gmail.com>
2018-02-08 22:40:26 -0500

    Uploaded Sample Unity Project

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform target;
+
+    public float distance = 10.0f;
+    public float height = 5.0f;
+
+    public float heightDamping = 2.0f;
+    public float rotationDamping = 3.0f;
+
+    Vector2 _lastPosition = Vector2.zero;
+    Camera cam;
+
+    private void Start()
+    {
+        cam = this.GetComponent<Camera>();
+    }
+
+    void LateUpdate()
+    {
+        if (!target) return;
+
+        if (Input.GetMouseButtonDown(1)) _lastPosition = Input.mousePosition;
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+            cam.fieldOfView = (cam.fieldOfView < 10) ? 10 : --cam.fieldOfView;
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+            cam.fieldOfView = (cam.fieldOfView > 60) ? 60 : ++cam.fieldOfView;
+
+
+        // Calculate the current rotation angles.
+        float wantedRotationAngle;
+        float wantedHeight;
+
+        if (Input.GetMouseButton(1)) {
+            wantedRotationAngle = target.eulerAngles.y + (_lastPosition.x - Input.mousePosition.x);
+            wantedHeight = target.position.y + height - (_lastPosition.y - Input.mousePosition.y);
+            wantedHeight = (wantedHeight > 10) ? 10 : (wantedHeight < 1 + ((60 - cam.fieldOfView) / 10)) ? 1 + ((60 - cam.fieldOfView) / 10) : wantedHeight;
+        }
+        else
+        {
+            wantedRotationAngle = target.eulerAngles.y;
+            wantedHeight = target.position.y + height;
+        }
+
+        float currentRotationAngle = transform.eulerAngles.y;
+        float currentHeight = transform.position.y;
+
+        // Damp the rotation around the y-axis
+        currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+
+        // Convert the angle into a rotation
+        var currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+
+        // Damp the height
+        currentHeight = Mathf.Lerp(currentHeight, wantedHeight - ((60 - cam.fieldOfView) / 10), heightDamping * Time.deltaTime);
+
+        // Set the position of the camera on the x-z plane to:
+        // distance meters behind the target
+        transform.position = target.position;
+        transform.position -= currentRotation * Vector3.forward * distance;
+
+        // Set the height of the camera
+        transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+
+        // Always look at the target
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour {
+
+    Animator anim;
+    int jumpHash = Animator.StringToHash("Jump");
+    int runStateHash = Animator.StringToHash("Base Layer.Run");
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        anim.SetFloat("Speed", z);
+
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+        if (Input.GetKeyDown(KeyCode.Space) && stateInfo.fullPathHash == runStateHash)
+        {
+            anim.SetTrigger(jumpHash);
+        }
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}

dcd1e997e9a8cd16990217ebdc094c134953b381
Guy Ponimansky <ponimansky.guy@gmail.com>
2018-02-09 22:37:34 -0500

    Added animations.

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -4,28 +4,24 @@ using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviour {
 
-    Animator anim;
+    public Animator anim;
     int jumpHash = Animator.StringToHash("Jump");
-    int runStateHash = Animator.StringToHash("Base Layer.Run");
-
-
-    void Start()
-    {
-        anim = GetComponent<Animator>();
-    }
+    int idleStateHash = Animator.StringToHash("Base Layer.Idle");
 	
 	// Update is called once per frame
 	void Update () {
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        var z = Input.GetAxis("Vertical");
 
-        anim.SetFloat("Speed", z);
+        if (Input.GetKey(KeyCode.LeftShift))
+            z *= 2;
+
+        anim.SetFloat("Vertical", z);
+        z *= Time.deltaTime * 3.0f;
 
         AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
-        if (Input.GetKeyDown(KeyCode.Space) && stateInfo.fullPathHash == runStateHash)
-        {
+        if (Input.GetKeyDown(KeyCode.Space) && stateInfo.fullPathHash == idleStateHash)
             anim.SetTrigger(jumpHash);
-        }
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
